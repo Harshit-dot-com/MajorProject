1b95b452397e07e4b4fa4474a4be10ffe4fa71c3
jaskirat <jaskiratsingh026@gmail.com>
2024-03-09 01:37:36 +0530

    nudity detector added

--- Nudenet.py
+++ Nudenet.py
@@ -1,3 +1,29 @@
 from nudenet import NudeDetector
-nude_detector = NudeDetector()
-nude_detector.detect('image.jpg')
+
+def NudityDetector(pathToImage):
+    nudityExists = False
+    nude_detector = NudeDetector()
+    preds = nude_detector.detect(pathToImage)
+    for pred in preds:
+        print(pred)
+        if(pred["class"] == "FEMALE_BREAST_EXPOSED"):
+            nudityExists = True
+            print("female breasts detected")
+        
+        if(pred["class"] == "BUTTOCKS_EXPOSED"):
+            nudityExists = True
+            print("buttocks detected")
+
+        if(pred["class"] == "FEMALE_GENITALIA_EXPOSED"):
+            nudityExists = True
+            print("Female genetailia detected")
+
+        if(pred["class"] == "ANUS_EXPOSED"):
+            nudityExists = True
+            print("Anus detected")
+
+        if(pred["class"] == "MALE_GENITALIA_EXPOSED"):
+            nudityExists = True
+            print("Male Genetalia detected")
+
+    return nudityExists
